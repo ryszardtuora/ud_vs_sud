468085114d683b3a37e6bd726d90e227b9fffe07
ryszardtuora <ryszardtuora@gmail.com>
2020-01-30 16:12:30 +0100

    Added the script for fetching the corpora

---
+++ get_treebanks.py
@@ -0,0 +1,28 @@
+import wget
+import tarfile
+from settings import UD_VERSION
+
+url_tb = "https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3105/ud-treebanks-v{}.tgz".format(UD_VERSION)
+url_docs = "https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3105/ud-documentation-v{}.tgz".format(UD_VERSION)
+url_tools = "https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-3105/ud-tools-v{}.tgz".format(UD_VERSION)
+
+print("downloading the UD docs...")
+wget.download(url_docs)
+print("downloading the UD tools...")
+wget.download(url_tools)
+print("downloading the UD corpora...")
+wget.download(url_tb)
+
+print("extracting the UD docs...")
+tf = tarfile.open("ud-documentation-v{}.tgz".format(UD_VERSION))
+tf.extractall()
+print("extracting the UD tools...")
+tf = tarfile.open("ud-tools-v{}.tgz".format(UD_VERSION))
+tf.extractall()
+print("extracting the UD corpora...")
+tf = tarfile.open("ud-treebanks-v{}.tgz".format(UD_VERSION))
+tf.extractall()
+
+
+
+
