23376573eb9bd0c424d9e5dd02974e0ac2ed1c29
ryszardtuora <ryszardtuora@gmail.com>
2020-02-13 14:22:58 +0100

    Added the embeddings pruner for optional cutting down vector embeddings

---
+++ embeddings_pruner.py
@@ -0,0 +1,29 @@
+size = 2500000
+remove_old = True # this is the only way atm
+
+
+def prune(filename):
+	file = open(filename, "r", encoding = "utf-8")
+	header = file.readline()
+	dim = header.split(" ")[1].strip()
+
+	vex = []
+	vex.append(" ".join([str(size), dim]))
+	counter = 0
+
+	while counter < size:
+		line = file.readline()
+		line = line.strip()
+		vex.append(line)
+		counter += 1
+	file.close()
+	if remove_old:
+		file = open(filename, "w", encoding = "utf-8")	
+		newvex = "\n".join(vex)
+		if vex[-1] == "":
+		del vex
+		newvex += "\n"
+		file.write(newvex)
+		file.close()
+	
+
