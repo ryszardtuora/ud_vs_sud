72611043f78d3effcc88f998d476c5f289e647bb
ALeczkowski <60295260+ALeczkowski@users.noreply.github.com>
2020-05-17 21:55:09 +0200

    Update mate_trainer.py

--- mate_trainer.py
+++ mate_trainer.py
@@ -4,6 +4,7 @@ import multiprocessing
 
 
 from good_treebanks import good_treebanks as treebanks
+from util import get_train_files
 
 nb_cores = (multiprocessing.cpu_count())/2
 
@@ -20,15 +21,18 @@ def train_mate(train_file):
         cmd = ["java", "-classpath Mate/anna-3.61.jar is2.parser.Parser", "-model {}".format(out_file), "-train {}".format(train_file), "-decodeTH {}".format(threshold), "-cores {}".format(nb_cores)]
         cmds.append((cmd, out_file))
     results = map(run_cli, cmds)
+    print(results)
+    joined_results = ["\n".join(t) for t in results]
+    txt = "\n\n".join(joined_results)
+
+    with open(train_file.replace("train.conllu", "udpipe_results.txt"), "w", encoding = "utf-8") as f:
+        f.write(txt)
     return results
-		
-def main(train_file):
-    results = train_mate(train_file)
-    tx = "\n\n".join(results)
 
-    with open(train_file.replace("train.conllu", "mate_results.txt"), "w", encoding = "utf-8") as f:
-        f.write(tx)
-
+def train_all():
+    for t in get_train_files():
+	t.replace("conllu", "conll09")
+        train_mate(t)
 
 
 
