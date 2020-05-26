a4cf3d81537a951216bdc370ea9e2a64773d628d
ALeczkowski <60295260+ALeczkowski@users.noreply.github.com>
2020-05-26 19:07:18 +0200

    Create get_parsers.py

---
+++ get_parsers.py
@@ -0,0 +1,24 @@
+import wget
+from os import mkdir, rename
+from zipfile import ZipFile
+
+url_mate_anna-3.61 = "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/mate-tools/anna-3.61.jar"
+url_udpipe-1.2.0 = "https://github.com/ufal/udpipe/releases/download/v1.2.0/udpipe-1.2.0-bin.zip"
+url_combo = "https://github.com/360er0/COMBO/archive/master.zip"
+
+print("downloading Mate graph-based parser v3.61")
+wget.download(url_mate_anna-3.61)
+print("downloading Udpipe v1.2.0")
+wget.download(url_udpipe-1.2.0)
+print("downloading COMBO")
+wget.download(url_combo)
+
+print("Moving Mate to a different folder")
+mkdir("Mate")
+rename("anna-3.61.jar", "Mate/anna-3.61.jar")
+print("Extracting Udpipe v.1.2.0")
+with ZipFile("udpipe-1.2.0-bin.zip") as zipObj1:
+    zipObj1.extractall()
+print("Extracting COMBO")
+with ZipFile("COMBO-master.zip") as zipObj2:
+    zipObj2.extractall()
