90b200f57dd4476e9f5ac867b6a253b2d10cdf7b
Aleksander Leczkowski <aleczkowski@mozart.ipipan.waw.pl>
2020-06-06 18:35:13 +0200

    fixing the converter

--- convert_to_09.py
+++ convert_to_09.py
@@ -1,9 +1,10 @@
 from os import path, listdir
 from conll09_converter import from_conllu_to_09
-
-from good_treebanks import good_treebanks as treebanks
+import json
 
 def convert_files():
+    with open('good_treebanks.json') as f:
+        treebanks = json.load(f)
     for scheme in ["ud", "sud"]:
         for t in treebanks:
             scheme_dir = path.join(scheme + "-treebanks-v2.5", t.replace("UD", scheme.upper()))
@@ -12,4 +13,5 @@ def convert_files():
                 if c.endswith("conllu"):
                     full_path = path.join(scheme_dir, c)
                     from_conllu_to_09(full_path)
-convert_files()
+if __name__ == "__main__":
+    convert_files()
