a24ab995334396b4b31101a6cbc343b8a290ee9e
Aleksander Leczkowski <aleczkowski@mozart.ipipan.waw.pl>
2020-06-06 18:15:58 +0200

    trainer updates

--- mate_trainer.py
+++ mate_trainer.py
@@ -1,3 +1,4 @@
+import os
 from os import path
 import subprocess
 import multiprocessing
@@ -21,9 +22,9 @@ def run_cli(args):
 def train_mate(train_file):
     cmds = []
     results = []
-    for threshold in [0.75, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01]:
+    for threshold in [0.75]:
         out_file = train_file.replace("train.conll09", "mate_{}.model".format(threshold))			      
-        cmd = ["java", "-classpath", "Mate/anna-3.61.jar", "is2.parser.Parser", "-model", out_file, "-train", train_file, "-decodeTH", str(threshold), "-cores", str(nb_cores)]
+        cmd = ["java", "-classpath", "anna-3.61.jar", "is2.parser.Parser", "-model", out_file, "-train", train_file, "-decodeTH", str(threshold), "-cores", str(nb_cores)]
         cmds.append((cmd, out_file))
     for cmd in cmds:
         result = run_cli(cmd)
@@ -41,27 +42,26 @@ def evaluate_mate(train_file):
     scores[train_file] = {}
     scores[train_file]['LAS'] = []
     scores[train_file]['UAS'] = []
-    for threshold in [0.75, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01]:
+    for threshold in [0.75]:
         out_file = train_file.replace("train.conll09", "mate_{}.conll09".format(threshold))
         dev_file = train_file.replace("train.conll09", "dev.conll09")
         model_file = train_file.replace("train.conll09", "mate_{}.model".format(threshold))
-        cmd = ["java", "-classpath", "Mate/anna-3.61.jar", "is2.parser.Parser", "-model", model_file, "-test", dev_file, "-out", out_file]
+        cmd = ["java", "-classpath", "anna-3.61.jar", "is2.parser.Parser", "-model", model_file, "-test", dev_file, "-out", out_file]
         out = subprocess.run(cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
         from_09_to_conllu(out_file)
         gold = load_conllu_file(dev_file.replace(".conll09", ".conllu"))
         sys = load_conllu_file(out_file.replace(".conll09", "_conv_back.conllu"))
         score = evaluate(gold, sys)
-        scores[train_file]['LAS'].append(score.get('LAS'))
-        scores[train_file]['UAS'].append(score.get('UAS'))
+        scores[train_file]['LAS'].append(score.get('LAS').f1)
+        scores[train_file]['UAS'].append(score.get('UAS').f1)
     return scores
 
 def choose_best(scores):
     reform = {k: pd.DataFrame(v) for k,v in scores.items()}
     df = pd.concat(reform, axis=1)
-    df.index = [0.75, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01]
+    df.index = [0.75]
     df.to_excel('results_mate_optimization.xlsx')
     chosen_models = []
-
     treebanks = df.columns.levels[0]
     for treebank in treebanks:
         col = df[treebank]
@@ -75,30 +75,43 @@ def choose_best(scores):
         
 def final_eval(chosen_models):
     scores = {}
+    scores_sorted = {}
+    scores_sorted["UAS"]["UD"] = []
+    scores_sorted["UAS"]["SUD"] = []
+    scores_sorted["LAS"]["UD"] = []
+    scores_sorted["LAS"]["SUD"] = []
+    name = []
     for model_file in chosen_models:
         scores[model_file] = {} 
         test_file = model_file.split("mate")[0] + "test.conll09"
         out_file = model_file.split("mate")[0] + "mate_final.conll09"
-        cmd = ["java", "-classpath", "Mate/anna-3.61.jar", "is2.parser.Parser", "-model", model_file, "-test", test_file, "-out", out_file]
+        cmd = ["java", "-classpath", "anna-3.61.jar", "is2.parser.Parser", "-model", model_file, "-test", test_file, "-out", out_file]
         out = subprocess.run(cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
         from_09_to_conllu(out_file)
         gold = load_conllu_file(test_file.replace(".conll09", ".conllu"))
         sys = load_conllu_file(out_file.replace(".conll09", "_conv_back.conllu"))
         score = evaluate(gold, sys)
-        scores[model_file]['UAS'] = score.get('UAS')
-        scores[model_file]['LAS'] = score.get('LAS')
+        scores[model_file]['UAS'] = score.get('UAS').f1
+        scores[model_file]['LAS'] = score.get('LAS').f1
+        if "sud" in model_file:
+            scores_sorted["UAS"]["SUD"].append(score.get('UAS').f1)
+            scores_sorted["LAS"]["SUD"].append(score.get('LAS').f1)
+        else:
+            scores_sorted["UAS"]["UD"].append(score.get('UAS').f1)
+            scores_sorted["LAS"]["UD"].append(score.get('LAS').f1)
+            name.append(os.path.basename(model_file).split("-")[0])
     results = pd.DataFrame.from_dict({i: scores[i] for i in scores.keys()}, orient='index')
-    results.to_excel('results_mate_final.xlsx')
+    results.to_csv('results_mate_final.csv')
+    results_sorted = pd.DataFrame.from_dict({(i, j): scores_sorted[i][j] for i in scores_sorted.keys() for j in scores_sorted[i].keys()})
+    results_sorted.index = name
+    results_sorted.to_csv('results_mate_final_sorted.csv')
 
 def train_all():
-    all_scores = []
+    all_scores = {}
     for t in get_train_files():
         t = t.replace("conllu", "conll09")
         train_mate(t)
-        scores = evaluate_mate(t) 
-        all_scores.append(scores)
-    score_table = pd.concat(all_scores, axis=1)
-    chosen_models = choose_best(score_table)
+        all_scores.update(evaluate_mate(t))
+    chosen_models = choose_best(all_scores)
     final_eval(chosen_models)
-    
-    
+train_all()    
--- udpipe_trainer.py
+++ udpipe_trainer.py
@@ -1,16 +1,22 @@
+import os
 from os import path, listdir
 import subprocess
 import multiprocessing
+import pandas as pd
 
 
-UDPIPE_RUNS = 4
+UDPIPE_RUNS = 7
 command_target = path.join("udpipe-1.2.0-bin", "bin-linux64", "udpipe")
+nb_of_processes = 21
 
-from good_treebanks import good_treebanks as treebanks
 from utils import get_train_files
+from conll18_ud_eval import evaluate, load_conllu_file
 
 
 "embeddings", "cc.{}.300.vec"
+
+def activate_udpipe():  
+    os.system("chmod u+x udpipe-1.2.0-bin/bin-linux64/udpipe")
 
 def run_cli(args):
     cmd, outname = args
@@ -23,13 +29,13 @@ def train_udpipe(train_file):
     cmds = []
     for i in range(1, UDPIPE_RUNS + 1):
         for system in ["projective", "swap", "link2"]:
-            out_file = train_file.replace("train.conllu", "udpipe_{}_{}.model".format(i, system))
+            out_file = train_file.replace("train.conllu", "udpipe_{}_{}.output".format(i, system))
             language = path.basename(train_file).split("_")[0]
             embeddings = path.join("embeddings", "cc.{}.300.vec".format(language))
             train_pars = "run={};transition_system={};embedding_form_file={}".format(i, system, embeddings)
             cmd = [command_target, "--train", "--tokenizer=none", "--tagger=none", "--parser", train_pars, out_file, "--heldout={}".format(dev_file), train_file]
             cmds.append((cmd, out_file))
-    with multiprocessing.Pool(4) as pool:
+    with multiprocessing.Pool(nb_of_processes) as pool:
         results = pool.map(run_cli, cmds)
         print(results)
     joined_results = ["\n".join(t) for t in results]
@@ -39,10 +45,83 @@ def train_udpipe(train_file):
         f.write(txt)
     return results
 
+def evaluate_udpipe(train_file):
+    scores = {}
+    scores[train_file] = {}
+    scores[train_file]['LAS'] = []
+    scores[train_file]['UAS'] = []
+    for i in range (1, UDPIPE_RUNS +1):
+        for system in ["projective", "swap", "link2"]:   
+            out_file = train_file.replace("train.conllu", "udpipe_{}_{}.conllu".format(i, system))
+            print(out_file)
+            dev_file = train_file.replace("train.conllu", "dev.conllu")
+            model_file = train_file.replace("train.conllu", "udpipe_{}_{}.output".format(i, system))
+            cmd = [command_target, "--parse", "--outfile={}".format(out_file), model_file, dev_file]
+            out = subprocess.run(cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+            gold = load_conllu_file(dev_file)
+            sys = load_conllu_file(out_file)
+            score = evaluate(gold, sys)
+            scores[train_file]['LAS'].append(score.get('LAS').f1)
+            scores[train_file]['UAS'].append(score.get('UAS').f1)
+    return scores
+
+def choose_best(scores):
+    reform = {k: pd.DataFrame(v) for k,v in scores.items()}
+    df = pd.concat(reform, axis=1)
+    df.index = [str(i) + "_" + system for i in range (1, UDPIPE_RUNS + 1) for system in ["projective", "swap", "link2"]]
+    df.to_excel('results_udpipe_optimization.xlsx')
+    chosen_models = []
+    treebanks = df.columns.levels[0]
+    for treebank in treebanks:
+        col = df[treebank]
+        las_col = col["LAS"]
+        uas_col = col["UAS"]
+        las_max_first_val = las_col[las_col.idxmax()]
+        las_maxes = col[lambda x: x["LAS"]==las_max_first_val]
+        uas_max = las_maxes["UAS"].idxmax()
+        chosen_models.append(treebank.replace("train.conllu", "udpipe_{}.output".format(uas_max)))
+    return chosen_models
+        
+def final_eval(chosen_models):
+    scores = {}
+    scores_sorted = {}
+    scores_sorted["UAS"] = {}
+    scores_sorted["LAS"] = {}
+    scores_sorted["UAS"]["UD"] = []
+    scores_sorted["UAS"]["SUD"] = []
+    scores_sorted["LAS"]["UD"] = []
+    scores_sorted["LAS"]["SUD"] = []
+    name = []
+    for model_file in chosen_models:
+        scores[model_file] = {} 
+        test_file = model_file.split("udpipe")[0] + "test.conllu"
+        out_file = model_file.split("udpipe")[0] + "udpipe_final.conllu"
+        cmd = [command_target, "--parse", "--outfile={}".format(out_file), model_file, test_file]
+        out = subprocess.run(cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+        gold = load_conllu_file(test_file)
+        sys = load_conllu_file(out_file)
+        score = evaluate(gold, sys)
+        scores[model_file]['UAS'] = score.get('UAS').f1
+        scores[model_file]['LAS'] = score.get('LAS').f1
+        if "sud" in model_file:
+            scores_sorted["UAS"]["SUD"].append(score.get('UAS').f1)
+            scores_sorted["LAS"]["SUD"].append(score.get('LAS').f1)
+        else:
+            scores_sorted["UAS"]["UD"].append(score.get('UAS').f1)
+            scores_sorted["LAS"]["UD"].append(score.get('LAS').f1)
+            name.append(os.path.basename(model_file).split("-")[0])
+    results = pd.DataFrame.from_dict({i: scores[i] for i in scores.keys()}, orient='index')
+    results.to_csv('results_udpipe_final.csv')
+    results_sorted = pd.DataFrame.from_dict({(i, j): scores_sorted[i][j] for i in scores_sorted.keys() for j in scores_sorted[i].keys()})
+    results_sorted.index = name
+    results_sorted.to_csv('results_udpipe_final_sorted.csv')
+
 def train_all():
+    activate_udpipe()
+    all_scores = {}
     for t in get_train_files():
         train_udpipe(t)
-
-
-# add external embeddings
-# add producing output on test
+        all_scores.update(evaluate_udpipe(t))
+    chosen_models = choose_best(all_scores)
+    final_eval(chosen_models)
+#train_all()    
